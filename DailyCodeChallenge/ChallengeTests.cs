a80facf0b9226d679816199494721f09e050ff60
vman77v <v_viayra@yahoo.com>
2017-03-31 08:59:14 -0600

    first challenge

---
+++ DailyCodeChallenge/ChallengeTests.cs
@@ -0,0 +1,27 @@
+﻿using DailyCodeChallenge;
+using NUnit.Framework;
+
+[TestFixture]
+public class ChallengeTest
+{
+    [Test]
+    public static void Test1()
+    {
+        int[] exampleTest1 = { 2, 6, 8, -10, 3 };
+        Assert.IsTrue(3 == Challenge.Find(exampleTest1));
+    }
+
+    [Test]
+    public static void Test2()
+    {
+        int[] exampleTest2 = { 206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781 };
+        Assert.IsTrue(206847684 == Challenge.Find(exampleTest2));
+    }
+
+    [Test]
+    public static void Test3()
+    {
+        int[] exampleTest3 = { int.MaxValue, 0, 1 };
+        Assert.IsTrue(0 == Challenge.Find(exampleTest3));
+    }
+}
---
+++ DailyCodeChallenge/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            //you can figure out the problem with just running the unit tests
+            //or call the method with a pre-populated variable to step through your logic
+        }
+    }
+}
